3d2267768cb72d23061b603eed1252f625caf436
panta134 <pants1854@gmail.com>
2026-01-21 16:03:58 -0600

    Fixed formatting issues

--- Class2.java
+++ Class2.java
@@ -8,14 +8,14 @@ public class Class2 {
     private void f1() {
         // v1.0
     }
-        private void f2(){
-            //v2.0
-        }
-            private void f3(){
-                //v3.0
-            }
-            private void f4(){
-                //v4.0
-            }
-        }
-    
+    private void f2(){
+        //v2.0
+    }
+    private void f3(){
+        //v3.0
+    }
+    private void f4(){
+        //v4.0
+    }
+
+}
